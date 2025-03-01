75215cfdf3f6caa9571d53e9744da89e68331b36
Dimitri Almon <dimitri.almon@gmail.com>
2025-03-01 02:31:28 +0100

    Adding few things

--- start_hermes.sh
+++ start_hermes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd /chemin/vers/ton/projet
+cd /root/MICRO-STORM
 docker build -t hermes .
 docker run -d --name hermes_container \
   -p 3630:3630 \
   -p 4567:4567 \
-  -e DB_NAME=ma_base \
-  -e DB_USER=mon_user \
-  -e DB_PASS=mon_pass \
+  -e DB_NAME=hermes \
+  -e DB_USER=user \
+  -e DB_PASS=admin \
   -e DB_HOST=localhost \
   hermes
